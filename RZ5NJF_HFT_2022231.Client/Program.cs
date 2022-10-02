01ec6474bc3563942f851bf06c1438cbe8cb0613
Balázs Lengyel <lengyel.balazs.t@gmail.com>
2022-10-02 14:15:17 +0200

    Add project files.

---
+++ RZ5NJF_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RZ5NJF_HFT_2022231.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RZ5NJF_HFT_2022231.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RZ5NJF_HFT_2022231.Logic
+{
+    public class Class1
+    {
+    }
+}
