bfdea8a47ba4f59be90d59246c9d1f9ff6043c60
Balázs Lengyel <lengyel.balazs.t@gmail.com>
2022-11-24 16:31:53 +0100

    Fixed enpoint project, added project references

---
+++ RZ5NJF_HFT_2022231.Endpoint/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RZ5NJF_HFT_2022231.Endpoint
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
