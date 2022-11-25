4bb7cc8b80d78732508891bf4a65051511b5ce10
Balázs Lengyel <lengyel.balazs.t@gmail.com>
2022-11-25 17:57:56 +0100

    created classes in Logic Projecz

---
+++ RZ5NJF_HFT_2022231.Logic/SmartPhoneOSLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RZ5NJF_HFT_2022231.Logic
+{
+    internal class SmartPhoneOSLogic
+    {
+    }
+}
