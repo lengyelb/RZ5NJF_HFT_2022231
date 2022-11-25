9bf2d11dd300e465d2b9f3192f196f7a5a85f0ef
Balázs Lengyel <lengyel.balazs.t@gmail.com>
2022-11-25 18:01:42 +0100

    Added CRUD methods to interfaces

--- RZ5NJF_HFT_2022231.Logic/IPhoneLogic.cs
+++ RZ5NJF_HFT_2022231.Logic/IPhoneLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RZ5NJF_HFT_2022231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace RZ5NJF_HFT_2022231.Logic
 {
-    internal class IPhoneLogic
+    public interface IPhoneLogic
     {
+        void Create(Phone phone_to_create);
+        void Delete(int phone_to_delete_id);
+        Phone Read(int phone_to_read_id);
+        IQueryable<Phone> ReadAll();
+        void Update(Phone phone_to_update);
     }
 }
--- RZ5NJF_HFT_2022231.Logic/ISmartPhoneOSLogic.cs
+++ RZ5NJF_HFT_2022231.Logic/ISmartPhoneOSLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RZ5NJF_HFT_2022231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace RZ5NJF_HFT_2022231.Logic
 {
-    internal class ISmartPhoneOSLogic
+    public interface ISmartPhoneOSLogic
     {
+        void Create(SmartPhoneOS os_to_create);
+        void Delete(int os_to_delete_id);
+        SmartPhoneOS Read(int os_to_read_id);
+        IQueryable<SmartPhoneOS> ReadAll();
+        void Update(SmartPhoneOS os_to_update);
     }
+}
 }
