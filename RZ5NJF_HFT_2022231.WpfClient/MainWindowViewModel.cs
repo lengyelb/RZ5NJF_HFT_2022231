e3c76a062ae60381d9438243167e5d72d7ede148
lengyelb <lengyel.balazs.t@gmail.com>
2023-05-08 17:53:11 +0200

    Created WPF listbox to show the companies

---
+++ RZ5NJF_HFT_2022231.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RZ5NJF_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RZ5NJF_HFT_2022231.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Company> Companies { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Companies = new RestCollection<Company>("http://localhost:22184/", "company");
+        }
+    }
+}
